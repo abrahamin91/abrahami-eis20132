88b391aa1831b451ff43165241c49837aa827a81
abrahamin91 <abrahamin91@gmail.com>
2013-09-23 17:51:02 -0300

    renombrar archivos

--- ejercicios/katas-parte-2/tenis_spec.rb
+++ ejercicios/katas-parte-2/tenis_spec.rb
@@ -1,5 +1,5 @@
 require 'rspec'
-require_relative '/home/mint/abrahami-eis20132/ejercicios/katas-parte-2/TennisGame.rb'
+require_relative '/home/mint/abrahami-eis20132/ejercicios/katas-parte-2/tenis.rb'
 
 describe TennisGame do
 
@@ -17,11 +17,19 @@ describe TennisGame do
     end
 	
    it 'Selecciona el proximo lanzador' do
-      a = TennisGame.new("Abraham","love","lanzador")
+      a = TennisGame.new("Abraham",0,"lanzador")
       b = TennisGame.new("Luis",0,"receptor")
       a.elegir_lanzamiento(a,b)
       a.rol.should eq "receptor"
       b.rol.should eq "lanzador"
     end
+
+   it 'Muestra el Ganador' do
+      a = TennisGame.new("1",3,"lanzador")
+      b = TennisGame.new("2",0,"receptor")
+      (g1,g2) = a.verificar_ganador(a,b)
+      g1.should eq true
+      g2.should eq false
+   end
 end
 
